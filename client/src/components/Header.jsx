7151faedb07717716fe588a950bdf1c22cfceeb6
yulianto <jooliantoo@gmail.com>
2024-04-02 07:09:03 +0700

    first commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+        <span className="text-slate-500">Yulicom</span>
+        <span className="text-slate-700">Estate</span>
+      </h1>
+    </header>
+  );
+}
