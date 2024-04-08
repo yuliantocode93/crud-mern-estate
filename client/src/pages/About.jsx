fcb5ce5503667e1173e6c52ae01f5d912dbea7d0
yulianto <jooliantoo@gmail.com>
2024-04-08 10:41:02 +0700

    final commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Yulianto Estate</h1>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Yulianto Properties</h1>
       <p className="mb-4 text-slate-700">
         Yulianto Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and
         making the buying and selling process as smooth as possible.
