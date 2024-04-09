8aeed282c1ccfdca7ee1e477ddce5a0892471f8b
yulianto <jooliantoo@gmail.com>
2024-04-09 13:33:55 +0700

    revisi about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,8 +5,8 @@ export default function About() {
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">About Yulianto Properties</h1>
       <p className="mb-4 text-slate-700">
-        Yulianto Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and
-        making the buying and selling process as smooth as possible.
+        Yulianto Properties is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service
+        and making the buying and selling process as smooth as possible.
       </p>
       <p className="mb-4 text-slate-700">
         Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here
